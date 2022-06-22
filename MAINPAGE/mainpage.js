aa11b093ab81bd1a74f61021875fbd12f61e7643
André Leonardo <andrevargasgimenes1@gmail.com>
2022-06-22 00:55:29 -0300

    calculadorabinaria

--- MAINPAGE/mainpage.js
+++ MAINPAGE/mainpage.js
@@ -32,3 +32,45 @@ function mudarFundo(){
     }
 }
 
+window.addEventListener('load', bemVindo())
+
+function bemVindo() {
+    const params = (new URL(document.location)).searchParams;
+    const nome = params.get('name');
+    document.getElementById('nomeRegistrado').innerHTML = nome;
+    if(nome==null){
+        document.getElementById("nomeUser").style.visibility = "hidden";
+    }
+}
+
+
+document.getElementById("botaoCalculo").addEventListener('click', decimalToBinario)
+
+function decimalToBinario(){
+    
+    let calculo = document.getElementById("numero").value;
+    let arredondar = calculo.toString().split('.')[0];
+    let resposta = document.getElementById("resultadoBinario");
+    parseInt(arredondar);
+    let resultado = [];
+do
+    {
+        if(arredondar % 2 == 0)
+        {
+            resultado.push(0);
+        }
+        else
+        {
+            resultado.push(1);
+        }
+        arredondar = arredondar / 2;
+        arredondar = arredondar.toString().split('.')[0];
+        parseInt(arredondar);
+    }
+    while(arredondar != 0 )
+
+    resultado.reverse();
+    document.getElementById('resultadoBinario').innerHTML = '';
+    resposta.append(resultado)
+    console.log(resultado);
+}
